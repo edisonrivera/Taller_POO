469d36df8d1116b356936ebaf1fc44ce8a66adc9
EstelaIb <estelachibnz13@gmail.com>
2022-07-04 21:14:50 -0500

    Estela: Add Juegos y Archivo txt

---
+++ Juegos.java
@@ -0,0 +1,28 @@
+package extraerDatos;
+
+/*
+    Edison Rivera
+ */
+
+public class Juegos {
+    private String nombre;
+    private String categoria;
+    private String publico;
+
+    public String toString(){
+        return "- Nombre: " + this.nombre + "\n- Categoria: " + this.categoria + "\n- Publico: " +
+                this.publico;
+    }
+
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setCategoria (String categoria){
+        this.categoria = categoria;
+    }
+
+    public void setPublico (String publico){
+        this.publico = publico;
+    }
+}
